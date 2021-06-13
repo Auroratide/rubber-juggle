227ba4abd0bb3493dcc42283a480011c20a78871
Timothy Foster <tf.auroratide@gmail.com>
2021-06-13 12:14:48 -0500

    Pegs die faster and faster

--- game/PegGenerator.ts
+++ game/PegGenerator.ts
@@ -6,13 +6,16 @@ import { SoundManager } from './SoundManager'
 export class PegGenerator {
     private board: Pegboard
     private task: RepeatingTask
+    private generation: number
 
     constructor(board: Pegboard, ticker: PIXI.Ticker) {
         this.board = board
+        this.generation = 0
         this.task = new RepeatingTask(ticker, () => {
+            this.generation += 1
             this.board.makeRandomPeg()
             this.board.killOldestPeg()
-        }, () => Math.random() * 2500 + 2500)
+        }, () => Math.random() * 2500 + 2500 - Math.min(1500, this.generation * 50))
     }
 
     start = () => {
